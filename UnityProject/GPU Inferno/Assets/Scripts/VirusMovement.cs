483171b50287bf08ce1a9766f59cbd7ce45f3f98
César Morillo <moriceltarra@gmail.com>
2025-02-05 22:32:43 +0100

    chore: Add Unity NavMeshComponents and update dependencies

--- UnityProject/GPU Inferno/Assets/Scripts/VirusMovement.cs
+++ UnityProject/GPU Inferno/Assets/Scripts/VirusMovement.cs
@@ -1,36 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class VirusMovement : MonoBehaviour
 {
    public Transform target; // El objetivo al que se moverá el personaje
     public float speed = 5f; // Velocidad de movimiento
+    NavMeshAgent agent; // Componente NavMeshAgent
 
     void Start() {
         target = GameObject.Find("Capsule").transform;
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
     }
 
     void Update()
-    {
-        if (target != null)
-    {
-        // Mueve hacia el jugador
-        Vector3 direction = (target.position - transform.position).normalized;
-        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-
-        // Separación de otros enemigos cercanos
-        Collider[] nearbyEnemies = Physics.OverlapSphere(transform.position, 1f);  // Radio de detección
-
-        foreach (Collider enemy in nearbyEnemies)
         {
-            if (enemy.gameObject != this.gameObject)  // Asegurarse de que no se detecte a sí mismo
-            {
-                Vector3 pushDirection = transform.position - enemy.transform.position;
-                transform.position += pushDirection.normalized * 0.1f;  // Empujar ligeramente para evitar la superposición
-            }
+            if (target != null)
+        {
+            agent.SetDestination(target.position);
+            agent.speed = speed;       
         }
-    }
     }
     
 }

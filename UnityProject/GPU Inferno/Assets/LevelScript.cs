8cd839d19b16a6ddb8da4691896a86ad10a316a0
moriceltarra <118289796+moriceltarra@users.noreply.github.com>
2025-03-06 17:40:53 +0100

    chore: Add new LevelBar animation and LevelScript functionality; create new script file

---
+++ UnityProject/GPU Inferno/Assets/LevelScript.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelScript : MonoBehaviour
+{
+    public Image[] levelImages;
+    private int level = 0;
+    private int barLevel = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < levelImages.Length; i++)
+        {
+            levelImages[i].enabled = false;
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Si pulsas espacio sube de nivel
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Subir de nivel");
+            Debug.Log("Nivel: " + level);
+            Debug.Log("Barra de nivel: " + barLevel);
+            barLevel++;
+            if (barLevel > levelImages.Length)
+            {
+                for (int i = 0; i < levelImages.Length; i++)
+                {
+                    levelImages[i].enabled = false;
+
+                }
+                barLevel = 0;
+                level++;
+            }
+            else
+            {
+                levelImages[barLevel - 1].enabled = true;
+            }
+
+        }
+    }
+}
